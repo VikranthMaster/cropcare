0eaebbd9ed5079150b1d7b771e9c0cd2e98bc54c
VikranthMaster <vikrantht32@gmail.com>
2024-11-16 07:43:26 +0530

    second

--- actual/main.py
+++ actual/main.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template,request, redirect, url_for
+from flask import Flask, render_template,request, redirect, url_for, flash
+import os
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
 import requests
 from bs4 import BeautifulSoup
+from werkzeug.utils import secure_filename
+from wtforms.validators import InputRequired
 
 
 
@@ -15,6 +20,19 @@ def search(q):
 
 app= Flask(__name__)
 app.config['SECRET_KEY'] = 'asndjasnd'
+app.config['UPLOAD_FOLDER'] = os.path.join('static', 'uploads')
+app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
+
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+
+class UploadFileForm(FlaskForm):
+    file = FileField("File", validators=[InputRequired()])
+    submit = SubmitField("Upload")
+
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -40,6 +58,33 @@ def home():
             return render_template("test.html")
     
     return render_template('test.html')
+
+@app.route('/ai', methods=['GET', 'POST'])
+def index():
+    # if request.method == 'POST':
+    #     if 'file' not in request.files:
+    #         flash('No file part')
+    #         return redirect(request.url)
+    #     file = request.files['file']
+    #     if file.filename == '':
+    #         flash('No selected file')
+    #         return redirect(request.url)
+    #     if file and allowed_file(file.filename):
+    #         filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    #         file.save(filepath)
+    #         flash('File uploaded successfully!')
+    #         return render_template('ai.html', uploaded_file=file.filename)
+    # return render_template('ai.html')
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
+        return "File has been uploaded"
+    return render_template("ai.html", form=form)
+
+@app.route('/ai/uploads/<filename>')
+def uploaded_file(filename):
+    return redirect(url_for('static', filename=f'uploads/{filename}'))
 
 if __name__ == '__main__':
     app.run(debug=True)
